65ea293f7e5c9b15f868e9871768a8cd12ab254f
AndreyM29 <146368626+AndreyM29@users.noreply.github.com>
2023-10-05 19:08:55 +0300

    Update ping-pong.py
    Добавлены платформы и мяч. Обработаны: автоматическое движение мяча, передвижение платформ, столкновение мяча с платформами и с границами окна, условия проигрыша.

--- ping-pong.py
+++ ping-pong.py
@@ -19,12 +19,13 @@ class Player(GameSprite):
         if keys_pressed[K_s] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
     def update_r(self):
+        keys_pressed = key.get_pressed()
         if keys_pressed[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys_pressed[K_DOWN] and self.rect.y < win_width - 80:
             self.rect.y += self.speed
 
-win_width = 700
+win_width = 470
 win_height = 500
 window = display.set_mode((700, 500))
 display.set_caption('Пинг-понг')
@@ -32,18 +33,52 @@ display.set_caption('Пинг-понг')
 back = (45, 207, 147)
 window.fill(back)
 
-ball = GameSprite(225, 425, 'ball.png', 50, 50)
-ball.draw()
+ball = GameSprite('ball.png', 200, 100, 3, 60, 60)
 
-platform = GameSprite(200, 475, 'platform.png', 100, 30)
-platform.draw()
+platform_1 = Player('platform.png', 550, 200, 3, 70, 100)
 
-game = True 
+platform_2 = Player('platform.png', 100, 200, 3, 70, 100)
+
+font.init()
+font_1 = font.Font(None, 35)
+lose_1 = font_1.render('PLAYER 1 LOSE!', True, (100,0,0))
+font_2 = font.Font(None, 35)
+lose_2 = font_2.render('PLAYER 2 LOSE!', True, (100,0,0))
+
+speed_x = 3
+speed_y = 3
+
+game = True
+finish = False 
 clock = time.Clock()
 FPS = 60
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
+    if finish != True:
+        window.fill(back)
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y 
+
+        platform_1.update_r()
+        platform_2.update_l()
+
+        platform_1.reset()
+        platform_2.reset()
+        ball.reset()
+
+        if sprite.collide_rect(platform_1, ball) or sprite.collide_rect(platform_2, ball):
+            speed_x *= -1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose_1, (200,200))
+        if ball.rect.x > 640:
+            finish = True
+            window.blit(lose_2, (200,200))
+
     display.update()
     clock.tick(FPS)
